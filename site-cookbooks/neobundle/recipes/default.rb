81768cbc3955f32bdc9af1d9d33bb68f8077d38a
U-r_nishio-PC\r_nishio <r_nishio@r_nishio-PC.(none)>
2014-03-29 23:21:47 +0900

    neobundle

---
+++ site-cookbooks/neobundle/recipes/default.rb
@@ -0,0 +1,38 @@
+#
+# Cookbook Name:: neobundle
+# Recipe:: default
+#
+
+BUNDLE_HOME = "/home/" + node["neobundle"]["user"] + "/.vim/bundle"
+USER = node["neobundle"]["user"]
+GROUP = node["neobundle"]["group"]
+
+directory BUNDLE_HOME do
+	owner USER
+	group GROUP
+	mode 744
+	action :create
+	recursive ture
+end
+
+git BUNDLE_HOME + "/neobundle.vim/" do
+	user USER
+	group GROUP
+	repository "git://github.com/Shougo/neobundle.vim"
+	reference master
+	action :sync
+end
+
+bash "chown" do
+	cwd BUNDLE_HOME
+	code "chown -R " << USER << ":" << GROUP << " " << BUNDLE_HOME << "&&" << "chmod -R 744 " << BUNDLE_HOME
+end
+
+templete ".vimrc" do
+	path BUNDLE_HOME + "/../../.vimrc"
+	source "vimrc.erb"
+	owner USER
+	group GROUP
+	mode 744
+end
+
