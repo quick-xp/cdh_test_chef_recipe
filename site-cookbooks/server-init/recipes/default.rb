934fc335855228c035254060c4ecc97e209b7b40
rnishio <quick.xp@me.com>
2014-03-31 01:50:40 +0900

    zshrc

--- site-cookbooks/server-init/recipes/default.rb
+++ site-cookbooks/server-init/recipes/default.rb
@@ -5,6 +5,10 @@
 
 USER = node["server-init"]["user"]
 GROUP = node["server-init"]["group"]
+HOME = '/home/' + USER
+
+# install zsh
+package 'zsh'
 
 group GROUP do
 	group_name GROUP
@@ -12,16 +16,42 @@ group GROUP do
 end
 
 user USER do
-	shell '/bin/bash'
+	shell '/bin/zsh'
 	home '/home/' + USER
 	password nil
 	supports :manage_home => true, :non_unique => false
 	action   [:create]
 end
 
-directory '/home/' + USER do
+directory HOME do
 	owner USER
 	group GROUP
 	mode '0775'
 	action :create
 end
+
+templete ".zshrc" do
+	path HOME << "/.zshrc"
+	source "zshrc.erb"
+	owner USER
+	group GROUP
+	mode 00744
+end
+
+directory HOME << "/plugin" do
+	user USER
+	group GROUP
+	mode 744
+	action :create
+	recursive true
+end
+
+git HOME << "/plugin" do
+	user USER
+	group GROUP
+	repository "https://github.com/hchbaw/auto-fu.zsh.git"
+	revision "master"
+	action :checkout
+end
+
+	
