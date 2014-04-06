420568bebec0f230afc2fc48bbfd574edd307cd7
rnishio <quick.xp@me.com>
2014-04-06 17:57:27 +0900

    hadoop mod

--- site-cookbooks/hadoop/attributes/default.rb
+++ site-cookbooks/hadoop/attributes/default.rb
@@ -1,2 +1,2 @@
-default['hadoop']['url'] = 'http://ftp.kddilabs.jp/infosystems/apache/hadoop/common/hadoop-2.3.0/hadoop-2.3.0.tar.gz'
-default['hadoop']['filename'] = 'hadoop-2.3.0'
+default['hadoop']['url'] = 'http://ftp.kddilabs.jp/infosystems/apache/hadoop/common/hadoop-2.2.0/hadoop-2.2.0.tar.gz'
+default['hadoop']['filename'] = 'hadoop-2.2.0'
